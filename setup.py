10fddf2236491b7c778cccb40f73397900028711
Ricardo de Azambuja <ricardo.azambuja@gmail.com>
2020-04-17 14:28:39 -0400

    Just to change the version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="Colab_Utils",
-    version="0.1",
+    name="colab_utils",
+    version="0.2",
     packages=['colab_utils'],
     install_requires=['ffmpeg-python', 'scipy', 'pillow', 'numpy'],
 
@@ -9,7 +9,7 @@ setup(
     author="Ricardo de Azambuja",
     author_email="ricardo.azambuja@gmail.com",
     description="Some useful (or not so much) Python stuff for Google Colab notebooks",
-    keywords="Notebook colab colaboratory google Numpy PIL",
+    keywords="Notebook colab colaboratory google Numpy PIL OpenCV",
     url="https://github.com/ricardodeazambuja/colab_utils",
     classifiers=[
         'Programming Language :: Python :: 3 :: Only' # https://pypi.org/classifiers/
